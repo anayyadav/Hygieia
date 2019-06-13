f63e25664001f190cc80fad3fa1988b4b919cb89
Yadav, Anay (VM) <anay.yadav@erieinsurance.com>
2019-06-13 09:32:24 +0000

    added a new collecotr for Mendix

---
+++ collectors/Mendix/mendix/config.py
@@ -0,0 +1,7 @@
+import os
+Mendix_collector = {
+        'username':os.getenv("MENDIX_USER", "world") ,
+        'apikey': os.getenv("MENDIX_KEY", "world"),
+        'db_host': os.getenv("DB_HOST", "db"),
+        'db_port': 27017
+        }
