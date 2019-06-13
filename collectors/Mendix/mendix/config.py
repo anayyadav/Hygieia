0ee10d02b5264f33d5a75fe6af766caa224c49ad
Yadav, Anay (VM) <anay.yadav@erieinsurance.com>
2019-06-13 09:45:46 +0000

    Hygieia Updated

--- collectors/Mendix/mendix/config.py
+++ collectors/Mendix/mendix/config.py
@@ -1,6 +1,6 @@
 import os
 Mendix_collector = {
-        'username':os.getenv("MENDIX_USER", "world") ,
+        'username': os.getenv("MENDIX_USER", "world"),
         'apikey': os.getenv("MENDIX_KEY", "world"),
         'db_host': os.getenv("DB_HOST", "db"),
         'db_port': 27017
