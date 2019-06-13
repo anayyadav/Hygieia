a703aecc92483e019a8139b43f079ba8c5a07e99
Yadav, Anay (VM) <anay.yadav@erieinsurance.com>
2019-06-13 09:23:07 +0000

    updated

--- collectors/AgmCollector/AGM/config.py
+++ collectors/AgmCollector/AGM/config.py
@@ -3,6 +3,6 @@ Agilemanager_collector = {
         'AGM_SERVER': os.getenv("AGM_SERVER", "world"),
         'client_id': os.getenv("CLIENT_ID", "world"),
         'client_secret': os.getenv("CLIENT_SECRET", "world"),
-        'db_host': 'localhost',
+        'db_host': os.getenv("DB_HOST","db"),
         'db_port': 27017
         }
